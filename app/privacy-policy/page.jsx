319ca1cf219a636478d5eee1e1e747628f2175d6
Polashahmad01 <dev.polashahmad@gmail.com>
2023-12-28 00:42:43 +0600

    Add new pages and update layout design

---
+++ app/privacy-policy/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Privacy Policy Page</h1>
+    </main>
+  )
+}
---
+++ app/testimonials/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Testimonial Page</h1>
+    </main>
+  )
+}
